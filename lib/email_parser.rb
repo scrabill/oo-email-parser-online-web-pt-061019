6a9f382a6d4d1852bdaf4e1cb9c0a7394705804f
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-14 01:50:03 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,53 @@
-# Build a class EmailParser that accepts a string of unformatted 
+# Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+require "pry"
+
+class EmailParser
+
+  attr_accessor :email
+  #
+  def initialize(string)
+    @string = string
+  end
+  #
+  def parse
+    array = []
+
+    # array << @string.split(" ")
+    # array << @string.split(", ")
+    # array << @string.split(",")
+    # array << @string.split(" ")
+
+    # array << @string.split(/[,|\s|,\s]+[^\s\b|]/) # What do the [] do?
+
+    # array << @string.split(/[,|\s][,\s]/)
+
+    # This kind of works
+    # array << @string.split(/,/)
+    # array << @string.split(/\s/)
+
+    array << @string.split(/,\s|\s|,/)
+    # binding.pry
+    # array << @string.split(/\s/)œœ
+    # array << @string.split(/,/)
+
+    # array.each do |item|
+    #   x = item.gsub(".com,",".com")
+    #   array << item
+    # end
+    # array.reject(&:empty?)
+
+    # array.reject! { |item| item.to_s.empty? }
+
+    array.flatten.uniq
+
+    # x = @string.split(" ")
+    # y = @string.split(",")
+    # z = x + y
+    # array = z.uniq
+  end
+
+end
