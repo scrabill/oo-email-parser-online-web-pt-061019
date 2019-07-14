a753f3ee853aa366c8c296bcf0417e0d08eb2ab7
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-14 01:25:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -31,7 +31,7 @@ class EmailParser
     array << @string.split(/\s/)
 
     array.each do |item|
-      item.delete(",")
+      item.trim(",")
       array << x
     end
     # array.reject(&:empty?)
